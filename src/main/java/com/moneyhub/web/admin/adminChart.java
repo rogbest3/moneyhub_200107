6c29643faa1c21877954838e0e86d132ed2acb68
김민국 <rogbest3@gmail.com>
2020-01-20 11:37:25 +0900

    test ok
    동작확인 ok

--- src/main/java/com/moneyhub/web/admin/adminChart.java
+++ src/main/java/com/moneyhub/web/admin/adminChart.java
@@ -1,5 +1,20 @@
 package com.moneyhub.web.admin;
 
-public class adminChart {
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Lazy
+@Data
+@Component
+@AllArgsConstructor
+@NoArgsConstructor
+public class AdminChart {
+	
+	private String cemail, cpwd, cno, age;
+	
 
 }
