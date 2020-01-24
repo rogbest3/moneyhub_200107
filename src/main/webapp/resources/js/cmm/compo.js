9ab4dc258f055139a1f041737617c59dfb6c6f6d
양현미 <53929448+hmyanghm@users.noreply.github.com>
2020-01-24 23:37:23 +0900

    200124 23:37

--- src/main/webapp/resources/js/cmm/compo.js
+++ src/main/webapp/resources/js/cmm/compo.js
@@ -29,14 +29,14 @@ compo =(()=>{
 		.html(compo_vue.compo(img))
 		$('html').scrollTop(0)
 		
-		$('.')
+		alert('과연?!')
 		var mapContainer = document.getElementById('map'), // 지도를 표시할 div
 	    mapOption = { 
 	        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
 	        level: 3 // 지도의 확대 레벨
 	    };
 
-		var map = new kakao.maps.Map(mapContainer, mapOption);
+		var map = new kakao.maps.Map(mapContainer, mapOption); //지도 생성 및 객체 리턴
 	
 		// 마커가 표시될 위치입니다
 		var markerPosition  = new kakao.maps.LatLng(33.450701, 126.570667); 
