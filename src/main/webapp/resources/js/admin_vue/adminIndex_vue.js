a3f8efca2266c3526544e4ce1d35df57831022c1
이은지 <gkry123@naver.com>
2020-02-12 10:23:02 +0900

    admin_mergy

--- src/main/webapp/resources/js/admin_vue/adminIndex_vue.js
+++ src/main/webapp/resources/js/admin_vue/adminIndex_vue.js
@@ -53,10 +53,6 @@ adminIndex_vue ={
 		'          <i class="fas fa-fw fa-home"></i>'+
 		'          <span>로그아웃</span>'+
 		'        </a>'+
-		'        <a class="nav-link collapsed" id="cusmain" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">'+
-		'          <i class="fas fa-fw fa-chart-area"></i>'+
-		'          <span>회원 메인</span>'+
-		'        </a>'+
 		'     <!-- Divider -->'+
 		'      <hr class="sidebar-divider d-none d-md-block">'+
 		'     <!-- Sidebar Toggler (Sidebar) -->'+
@@ -94,137 +90,6 @@ adminIndex_vue ={
 		'                    </div>'+
 		'                  </div>'+
 		'                </form>'+
-		'              </div>'+
-		'            </li>'+
-		'           <!-- Nav Item - Alerts -->'+
-		'            <li class="nav-item dropdown no-arrow mx-1">'+
-		'              <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">'+
-		'                <i class="fas fa-bell fa-fw"></i>'+
-		'                <!-- Counter - Alerts -->'+
-		'                <span class="badge badge-danger badge-counter">3+</span>'+
-		'              </a>'+
-		'              <!-- Dropdown - Alerts -->'+
-		'              <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="alertsDropdown">'+
-		'                <h6 class="dropdown-header">'+
-		'                  Alerts Center'+
-		'                </h6>'+
-		'                <a class="dropdown-item d-flex align-items-center" href="#">'+
-		'                  <div class="mr-3">'+
-		'                    <div class="icon-circle bg-primary">'+
-		'                      <i class="fas fa-file-alt text-white"></i>'+
-		'                    </div>'+
-		'                  </div>'+
-		'                  <div>'+
-		'                    <div class="small text-gray-500">December 12, 2019</div>'+
-		'                    <span class="font-weight-bold">A new monthly report is ready to download!</span>'+
-		'                  </div>'+
-		'                </a>'+
-		'                <a class="dropdown-item d-flex align-items-center" href="#">'+
-		'                  <div class="mr-3">'+
-		'                    <div class="icon-circle bg-success">'+
-		'                      <i class="fas fa-donate text-white"></i>'+
-		'                    </div>'+
-		'                  </div>'+
-		'                  <div>'+
-		'                    <div class="small text-gray-500">December 7, 2019</div>'+
-		'                    $290.29 has been deposited into your account!'+
-		'                  </div>'+
-		'                </a>'+
-		'                <a class="dropdown-item d-flex align-items-center" href="#">'+
-		'                  <div class="mr-3">'+
-		'                    <div class="icon-circle bg-warning">'+
-		'                      <i class="fas fa-exclamation-triangle text-white"></i>'+
-		'                    </div>'+
-		'                  </div>'+
-		'                  <div>'+
-		'                    <div class="small text-gray-500">December 2, 2019</div>'+
-		'                    Spending Alert: Weve noticed unusually high spending for your account.'+
-		'                  </div>'+
-		'                </a>'+
-		'                <a class="dropdown-item text-center small text-gray-500" href="#">Show All Alerts</a>'+
-		'              </div>'+
-		'            </li>'+
-		'           <!-- Nav Item - Messages -->'+
-		'            <li class="nav-item dropdown no-arrow mx-1">'+
-		'              <a class="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">'+
-		'                <i class="fas fa-envelope fa-fw"></i>'+
-		'                <!-- Counter - Messages -->'+
-		'                <span class="badge badge-danger badge-counter">7</span>'+
-		'              </a>'+
-		'              <!-- Dropdown - Messages -->'+
-		'              <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="messagesDropdown">'+
-		'                <h6 class="dropdown-header">'+
-		'                  Message Center'+
-		'                </h6>'+
-		'                <a class="dropdown-item d-flex align-items-center" href="#">'+
-		'                  <div class="dropdown-list-image mr-3">'+
-		'                    <img class="rounded-circle" src="https://source.unsplash.com/fn_BT9fwg_E/60x60" alt="">'+
-		'                    <div class="status-indicator bg-success"></div>'+
-		'                  </div>'+
-		'                  <div class="font-weight-bold">'+
-		'                    <div class="text-truncate">Hi there! I am wondering if you can help me with a problem Ive been having.</div>'+
-		'                    <div class="small text-gray-500">Emily Fowler · 58m</div>'+
-		'                  </div>'+
-		'                </a>'+
-		'                <a class="dropdown-item d-flex align-items-center" href="#">'+
-		'                  <div class="dropdown-list-image mr-3">'+
-		'                    <img class="rounded-circle" src="https://source.unsplash.com/AU4VPcFN4LE/60x60" alt="">'+
-		'                    <div class="status-indicator"></div>'+
-		'                  </div>'+
-		'                  <div>'+
-		'                    <div class="text-truncate">I have the photos that you ordered last month, how would you like them sent to you?</div>'+
-		'                    <div class="small text-gray-500">Jae Chun · 1d</div>'+
-		'                  </div>'+
-		'                </a>'+
-		'                <a class="dropdown-item d-flex align-items-center" href="#">'+
-		'                  <div class="dropdown-list-image mr-3">'+
-		'                    <img class="rounded-circle" src="https://source.unsplash.com/CS2uCrpNzJY/60x60" alt="">'+
-		'                    <div class="status-indicator bg-warning"></div>'+
-		'                  </div>'+
-		'                  <div>'+
-		'                    <div class="text-truncate">Last months report looks great, I am very happy with the progress so far, keep up the good work!</div>'+
-		'                    <div class="small text-gray-500">Morgan Alvarez · 2d</div>'+
-		'                  </div>'+
-		'                </a>'+
-		'                <a class="dropdown-item d-flex align-items-center" href="#">'+
-		'                  <div class="dropdown-list-image mr-3">'+
-		'                    <img class="rounded-circle" src="https://source.unsplash.com/Mv9hjnEUHR4/60x60" alt="">'+
-		'                    <div class="status-indicator bg-success"></div>'+
-		'                  </div>'+
-		'                  <div>'+
-		'                    <div class="text-truncate">Am I a good boy? The reason I ask is because someone told me that people say this to all dogs, even if they arent good...</div>'+
-		'                    <div class="small text-gray-500">Chicken the Dog · 2w</div>'+
-		'                  </div>'+
-		'                </a>'+
-		'                <a class="dropdown-item text-center small text-gray-500" href="#">Read More Messages</a>'+
-		'              </div>'+
-		'            </li>'+
-		'           <div class="topbar-divider d-none d-sm-block"></div>'+
-		'           <!-- Nav Item - User Information -->'+
-		'            <li class="nav-item dropdown no-arrow">'+
-		'              <button class="nav-link dropdown-toggle" id="userDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">'+
-		'                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Valerie Luna</span>'+
-		'                <img class="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60">'+
-		'              </a>'+
-		'              <!-- Dropdown - User Information -->'+
-		'              <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">'+
-		'                <a class="dropdown-item" href="#">'+
-		'                  <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>'+
-		'                  Profile'+
-		'                </a>'+
-		'                <a class="dropdown-item" href="#">'+
-		'                  <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>'+
-		'                  Settings'+
-		'                </a>'+
-		'                <a class="dropdown-item" href="#">'+
-		'                  <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>'+
-		'                  Activity Log'+
-		'                </a>'+
-		'                <div class="dropdown-divider"></div>'+
-		'                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">'+
-		'                  <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>'+
-		'                  Logout'+
-		'                </a>'+
 		'              </div>'+
 		'            </li>'+
 		'         </ul>'+
@@ -299,7 +164,7 @@ adminIndex_vue ={
 		'              <div class="card shadow mb-4">'+
 		'                <!-- Card Header - Dropdown -->'+
 		'                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">'+
-		'                  <h6 class="m-0 font-weight-bold text-primary">월별 수수료 수익(단위 1만달러)</h6>'+
+		'                  <h6 class="m-0 font-weight-bold text-primary">일별 수수료 수익(단위 1만달러)</h6>'+
 		'                  <div class="dropdown no-arrow">'+
 		'                    <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">'+
 		'                      <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>'+
@@ -402,44 +267,34 @@ adminIndex_vue ={
 		'                </div>'+
 		'              </div>'+
 		'           </div>'+
-		'           <div class="col-lg-6 mb-4">'+
-		'             <!-- Illustrations -->'+
-		'              <div class="card shadow mb-4">'+
-		'                <div class="card-header py-3">'+
-		'                  <h6 class="m-0 font-weight-bold text-primary">Illustrations</h6>'+
-		'                </div>'+
-		'                <div class="card-body">'+
-		'                  <div class="text-center">'+
-		'                  </div>'+
-		'                  <p>Add some quality, svg illustrations to your project courtesy of <a target="_blank" rel="nofollow" href="https://undraw.co/">unDraw</a>, a constantly updated collection of beautiful svg images that you can use completely free and without attribution!</p>'+
-		'                  <a target="_blank" rel="nofollow" href="https://undraw.co/">Browse Illustrations on unDraw &rarr;</a>'+
-		'                </div>'+
-		'              </div>'+
-		'             <!-- Approach -->'+
-		'              <div class="card shadow mb-4">'+
-		'                <div class="card-header py-3">'+
-		'                  <h6 class="m-0 font-weight-bold text-primary">Development Approach</h6>'+
-		'                </div>'+
-		'                <div class="card-body">'+
-		'                  <p>SB Admin 2 makes extensive use of Bootstrap 4 utility classes in order to reduce CSS bloat and poor page performance. Custom CSS classes are used to create custom components and custom utility classes.</p>'+
-		'                  <p class="mb-0">Before working with this theme, you should become familiar with the Bootstrap framework, especially the utility classes.</p>'+
-		'                </div>'+
-		'              </div>'+
-		'           </div>'+
+//		'           <div class="col-lg-6 mb-4">'+
+//		'             <!-- Illustrations -->'+
+//		'              <div class="card shadow mb-4">'+
+//		'                <div class="card-header py-3">'+
+//		'                  <h6 class="m-0 font-weight-bold text-primary">Illustrations</h6>'+
+//		'                </div>'+
+//		'                <div class="card-body">'+
+//		'                  <div class="text-center">'+
+//		'                  </div>'+
+//		'                  <p>Add some quality, svg illustrations to your project courtesy of <a target="_blank" rel="nofollow" href="https://undraw.co/">unDraw</a>, a constantly updated collection of beautiful svg images that you can use completely free and without attribution!</p>'+
+//		'                  <a target="_blank" rel="nofollow" href="https://undraw.co/">Browse Illustrations on unDraw &rarr;</a>'+
+//		'                </div>'+
+//		'              </div>'+
+//		'             <!-- Approach -->'+
+//		'              <div class="card shadow mb-4">'+
+//		'                <div class="card-header py-3">'+
+//		'                  <h6 class="m-0 font-weight-bold text-primary">Development Approach</h6>'+
+//		'                </div>'+
+//		'                <div class="card-body">'+
+//		'                  <p>SB Admin 2 makes extensive use of Bootstrap 4 utility classes in order to reduce CSS bloat and poor page performance. Custom CSS classes are used to create custom components and custom utility classes.</p>'+
+//		'                  <p class="mb-0">Before working with this theme, you should become familiar with the Bootstrap framework, especially the utility classes.</p>'+
+//		'                </div>'+
+//		'              </div>'+
+//		'           </div>'+
 		'          </div>'+
 		'       </div>'+
 		'        <!-- /.container-fluid -->'+
 		'     </div>'+
-		'      <!-- End of Main Content -->'+
-		'     <!-- Footer -->'+
-		'      <footer class="sticky-footer bg-white">'+
-		'        <div class="container my-auto">'+
-		'          <div class="copyright text-center my-auto">'+
-		'            <span>Copyright &copy; Your Website 2019</span>'+
-		'          </div>'+
-		'        </div>'+
-		'      </footer>'+
-		'      <!-- End of Footer -->'+
 		'   </div>'+
 		'    <!-- End of Content Wrapper -->'+
 		' </div>'+
