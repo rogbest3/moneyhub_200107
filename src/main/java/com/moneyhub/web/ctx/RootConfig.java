bb3835faf9a0588e8e61beaff76bba6fc81c99de
김민국 <53929446+rogbest3@users.noreply.github.com>
2020-02-17 00:33:12 +0900

    t
    t

--- src/main/java/com/moneyhub/web/ctx/RootConfig.java
+++ src/main/java/com/moneyhub/web/ctx/RootConfig.java
@@ -20,10 +20,9 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 public class RootConfig {
 	@Bean
 	public DataSource dataSource() {
-		//dataSource.setUrl("jdbc:mysql://moneyhub.cfa9kh7porrz.ap-northeast-2.rds.amazonaws.com:3306/moneyhub?serverTimezone=UTC"); 형진 아마존
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-		dataSource.setUrl("jdbc:mysql://moneyhub.cbbtq2igl2zi.ap-northeast-2.rds.amazonaws.com:3306/moneyhub?serverTimezone=UTC");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/moneyhub?serverTimezone=UTC");
 		dataSource.setUsername("moneyhub");
 		dataSource.setPassword("moneyhub");
 		return dataSource;
